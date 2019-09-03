c9932ece36a73b7712db8a4b8d6b709092d856c5
dengni1 <2117207138@qq.com>
2019-09-03 17:02:10 +0800

    第一次提交

---
+++ OcdIcmoSplit/frmBHD.Designer.cs
@@ -0,0 +1,248 @@
+﻿namespace OcdIcmoSplit
+{
+    partial class frmBHD
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.splitContainer1 = new System.Windows.Forms.SplitContainer();
+            this.btnexport = new System.Windows.Forms.Button();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBoxPlaner = new System.Windows.Forms.TextBox();
+            this.textBoxItemNumber = new System.Windows.Forms.TextBox();
+            this.buttonSerach = new System.Windows.Forms.Button();
+            this.label4 = new System.Windows.Forms.Label();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.dateTimePicker2 = new System.Windows.Forms.DateTimePicker();
+            this.dateTimePicker1 = new System.Windows.Forms.DateTimePicker();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.backgroundWorker1 = new System.ComponentModel.BackgroundWorker();
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).BeginInit();
+            this.splitContainer1.Panel1.SuspendLayout();
+            this.splitContainer1.Panel2.SuspendLayout();
+            this.splitContainer1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // splitContainer1
+            // 
+            this.splitContainer1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.splitContainer1.Location = new System.Drawing.Point(0, 0);
+            this.splitContainer1.Margin = new System.Windows.Forms.Padding(4);
+            this.splitContainer1.Name = "splitContainer1";
+            this.splitContainer1.Orientation = System.Windows.Forms.Orientation.Horizontal;
+            // 
+            // splitContainer1.Panel1
+            // 
+            this.splitContainer1.Panel1.Controls.Add(this.btnexport);
+            this.splitContainer1.Panel1.Controls.Add(this.label5);
+            this.splitContainer1.Panel1.Controls.Add(this.label1);
+            this.splitContainer1.Panel1.Controls.Add(this.textBoxPlaner);
+            this.splitContainer1.Panel1.Controls.Add(this.textBoxItemNumber);
+            this.splitContainer1.Panel1.Controls.Add(this.buttonSerach);
+            this.splitContainer1.Panel1.Controls.Add(this.label4);
+            this.splitContainer1.Panel1.Controls.Add(this.comboBox1);
+            this.splitContainer1.Panel1.Controls.Add(this.label3);
+            this.splitContainer1.Panel1.Controls.Add(this.label2);
+            this.splitContainer1.Panel1.Controls.Add(this.dateTimePicker2);
+            this.splitContainer1.Panel1.Controls.Add(this.dateTimePicker1);
+            // 
+            // splitContainer1.Panel2
+            // 
+            this.splitContainer1.Panel2.Controls.Add(this.dataGridView1);
+            this.splitContainer1.Size = new System.Drawing.Size(931, 495);
+            this.splitContainer1.SplitterDistance = 110;
+            this.splitContainer1.SplitterWidth = 5;
+            this.splitContainer1.TabIndex = 0;
+            // 
+            // btnexport
+            // 
+            this.btnexport.Location = new System.Drawing.Point(811, 57);
+            this.btnexport.Name = "btnexport";
+            this.btnexport.Size = new System.Drawing.Size(102, 30);
+            this.btnexport.TabIndex = 20;
+            this.btnexport.Text = "导出";
+            this.btnexport.UseVisualStyleBackColor = true;
+            this.btnexport.Click += new System.EventHandler(this.btnexport_Click);
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(703, 25);
+            this.label5.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(52, 15);
+            this.label5.TabIndex = 19;
+            this.label5.Text = "计划员";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(384, 28);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(82, 15);
+            this.label1.TabIndex = 18;
+            this.label1.Text = "物料长编码";
+            // 
+            // textBoxPlaner
+            // 
+            this.textBoxPlaner.Location = new System.Drawing.Point(781, 25);
+            this.textBoxPlaner.Margin = new System.Windows.Forms.Padding(4);
+            this.textBoxPlaner.Name = "textBoxPlaner";
+            this.textBoxPlaner.Size = new System.Drawing.Size(132, 25);
+            this.textBoxPlaner.TabIndex = 17;
+            // 
+            // textBoxItemNumber
+            // 
+            this.textBoxItemNumber.Location = new System.Drawing.Point(485, 25);
+            this.textBoxItemNumber.Margin = new System.Windows.Forms.Padding(4);
+            this.textBoxItemNumber.Name = "textBoxItemNumber";
+            this.textBoxItemNumber.Size = new System.Drawing.Size(181, 25);
+            this.textBoxItemNumber.TabIndex = 16;
+            // 
+            // buttonSerach
+            // 
+            this.buttonSerach.Location = new System.Drawing.Point(688, 58);
+            this.buttonSerach.Margin = new System.Windows.Forms.Padding(4);
+            this.buttonSerach.Name = "buttonSerach";
+            this.buttonSerach.Size = new System.Drawing.Size(100, 29);
+            this.buttonSerach.TabIndex = 15;
+            this.buttonSerach.Text = "查询";
+            this.buttonSerach.UseVisualStyleBackColor = true;
+            this.buttonSerach.Click += new System.EventHandler(this.buttonSerach_Click);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(31, 28);
+            this.label4.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(67, 15);
+            this.label4.TabIndex = 14;
+            this.label4.Text = "生产车间";
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(141, 25);
+            this.comboBox1.Margin = new System.Windows.Forms.Padding(4);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(215, 23);
+            this.comboBox1.TabIndex = 13;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(381, 65);
+            this.label3.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(22, 15);
+            this.label3.TabIndex = 12;
+            this.label3.Text = "到";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(31, 65);
+            this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(97, 15);
+            this.label2.TabIndex = 11;
+            this.label2.Text = "计划开工从：";
+            // 
+            // dateTimePicker2
+            // 
+            this.dateTimePicker2.Location = new System.Drawing.Point(485, 58);
+            this.dateTimePicker2.Margin = new System.Windows.Forms.Padding(4);
+            this.dateTimePicker2.Name = "dateTimePicker2";
+            this.dateTimePicker2.Size = new System.Drawing.Size(181, 25);
+            this.dateTimePicker2.TabIndex = 10;
+            // 
+            // dateTimePicker1
+            // 
+            this.dateTimePicker1.Location = new System.Drawing.Point(141, 58);
+            this.dateTimePicker1.Margin = new System.Windows.Forms.Padding(4);
+            this.dateTimePicker1.Name = "dateTimePicker1";
+            this.dateTimePicker1.Size = new System.Drawing.Size(215, 25);
+            this.dateTimePicker1.TabIndex = 9;
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.AllowUserToDeleteRows = false;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.dataGridView1.Location = new System.Drawing.Point(0, 0);
+            this.dataGridView1.Margin = new System.Windows.Forms.Padding(4);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.ReadOnly = true;
+            this.dataGridView1.RowTemplate.Height = 23;
+            this.dataGridView1.Size = new System.Drawing.Size(931, 380);
+            this.dataGridView1.TabIndex = 0;
+            this.dataGridView1.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellContentClick);
+            // 
+            // frmBHD
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(931, 495);
+            this.Controls.Add(this.splitContainer1);
+            this.Margin = new System.Windows.Forms.Padding(4);
+            this.Name = "frmBHD";
+            this.Text = "车间安排统计数据";
+            this.Load += new System.EventHandler(this.frmBHD_Load);
+            this.splitContainer1.Panel1.ResumeLayout(false);
+            this.splitContainer1.Panel1.PerformLayout();
+            this.splitContainer1.Panel2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).EndInit();
+            this.splitContainer1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.SplitContainer splitContainer1;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.DateTimePicker dateTimePicker2;
+        private System.Windows.Forms.DateTimePicker dateTimePicker1;
+        private System.Windows.Forms.Button buttonSerach;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBoxPlaner;
+        private System.Windows.Forms.TextBox textBoxItemNumber;
+        private System.Windows.Forms.Button btnexport;
+        private System.ComponentModel.BackgroundWorker backgroundWorker1;
+    }
+}
---
+++ OcdIcmoSplit/frmBHD.cs
@@ -0,0 +1,234 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Microsoft.ApplicationBlocks.Data;
+using System.Collections;
+using System.IO;
+using System.Data.OleDb;
+using System.Reflection;
+using System.Threading;
+using System.Globalization;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+using Microsoft.CSharp.RuntimeBinder;
+using System.Diagnostics;
+
+namespace OcdIcmoSplit
+{
+    public partial class frmBHD : Form
+    {
+        [DllImport("user32.dll", SetLastError = true)]
+        private static extern int GetWindowThreadProcessId(IntPtr hWnd, out int lpdwProcessId);
+
+        public void KillSpecialExcel(Microsoft.Office.Interop.Excel.Application m_objExcel)
+        {
+            try
+            {
+                if (m_objExcel != null)
+                {
+                    int processId;
+                    frmBHD.GetWindowThreadProcessId(new IntPtr(m_objExcel.Hwnd), out processId);
+                    Process.GetProcessById(processId).Kill();
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        [CompilerGenerated]
+        private static class o__SiteContainer0
+        {
+            public static CallSite<Func<CallSite, object, Microsoft.Office.Interop.Excel.Worksheet>> p__Site1;
+        }
+
+        // 数据保存
+        DataTable myTable = new DataTable();
+       // List<String> list1 = new List<string>();
+
+        //string WorkShop;
+        //DateTime beginDate;
+        //DateTime endDate;
+
+
+        public frmBHD(string myWorkShop, DateTime mybeginDate, DateTime myendDate)
+        {
+            InitializeComponent();
+            this.comboBox1.Text = myWorkShop;
+            this.dateTimePicker1.Value = mybeginDate;
+            this.dateTimePicker2.Value = myendDate;
+        }
+
+        private void frmBHD_Load(object sender, EventArgs e)
+        {
+            InsertComShopID();
+            LoadData();
+
+        }
+
+        private void LoadData()
+        {
+            System.TimeSpan t3 = this.dateTimePicker2.Value - this.dateTimePicker1.Value;  //两个时间相减 。默认得到的是 两个时间之间的天数   得到：365.00:00:00 
+
+            double getDay = t3.TotalDays;
+
+            if (getDay < 50)
+            {
+
+                System.Configuration.Configuration config = System.Configuration.ConfigurationManager.OpenExeConfiguration(System.Configuration.ConfigurationUserLevel.None);
+                string strConn = config.AppSettings.Settings["connectionstring"].Value;//this.txtUrl.Text.Trim();
+                DataTable dt = SqlHelper.ExecuteDataset(strConn, "pr_xc_StatPC", this.dateTimePicker1.Value.ToShortDateString(), this.dateTimePicker2.Value.ToShortDateString(), this.comboBox1.Text,this.textBoxPlaner.Text.Trim(),this.textBoxItemNumber.Text.Trim()).Tables[0];
+                myTable = dt;
+                this.dataGridView1.DataSource = dt;
+            }
+            else
+            {
+                MessageBox.Show( "为了保证查看统计的速度，统计只能看50天的数据","金蝶提示");
+            }
+            
+        }
+
+        public void InsertComShopID()
+        {
+            System.Configuration.Configuration config = System.Configuration.ConfigurationManager.OpenExeConfiguration(System.Configuration.ConfigurationUserLevel.None);
+            string strConn = config.AppSettings.Settings["connectionstring"].Value;//this.txtUrl.Text.Trim();
+
+            this.comboBox1.Items.Clear();//清空ComBox
+            IDataReader dr = SqlHelper.ExecuteReader(strConn, CommandType.StoredProcedure, "pr_xc_getWorkShop");
+            while (dr.Read())
+            {
+                this.comboBox1.Items.Add(dr[0].ToString());//循环读取数据
+            }//end block while
+
+            dr.Close();//  关闭数据集
+            // DB.GetColse();//关闭数据库连接
+        }
+
+        private void buttonSerach_Click(object sender, EventArgs e)
+        {
+            LoadData();
+        }
+
+ 
+
+
+        private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+
+        }
+
+        
+
+        /// <summary>
+        /// 导出Excel
+        /// </summary>
+        /// <param name="saveFileDialog"></param>
+        public void ToExcel(SaveFileDialog saveFileDialog)
+        {
+            // saveFileDialog = new SaveFileDialog();
+            //Thread.Sleep(1000); 
+            saveFileDialog.Filter = "Execl files (*.xls)|*.xls";
+            saveFileDialog.FilterIndex = 0;
+            saveFileDialog.RestoreDirectory = true;
+            saveFileDialog.CreatePrompt = false;
+            saveFileDialog.Title = "导出文件保存路径";
+            System.TimeSpan t3 = this.dateTimePicker2.Value - this.dateTimePicker1.Value;
+            double days = t3.TotalDays;
+
+            List<String> list = new List<string>();
+
+            list.Add("产线");
+            list.Add("产品编码");
+            list.Add("产品");
+            list.Add("规格型号");
+            for (int i = 0; i < days; i++)
+            {
+                list.Add(this.dateTimePicker1.Value.AddDays(i).ToShortDateString());
+            }
+
+            DialogResult result = saveFileDialog.ShowDialog();
+            if (result == DialogResult.OK)
+            {
+                string fileName = saveFileDialog.FileName;
+                if (myTable.Rows.Count == 0)
+                {
+                    return;
+                }
+                if (fileName.Length != 0)
+                {
+                    Missing value = Missing.Value;
+                    Microsoft.Office.Interop.Excel.Application application = (Microsoft.Office.Interop.Excel.Application)Activator.CreateInstance(Type.GetTypeFromCLSID(new Guid("00024500-0000-0000-C000-000000000046")));
+                    application.Visible = false;
+                    CultureInfo currentCulture = Thread.CurrentThread.CurrentCulture;
+                    Thread.CurrentThread.CurrentCulture = new CultureInfo("en-US");
+                    Microsoft.Office.Interop.Excel.Workbook workbook = application.Workbooks.Add(value);
+                    if (frmBHD.o__SiteContainer0.p__Site1 == null)
+                    {
+                        frmBHD.o__SiteContainer0.p__Site1 = CallSite<Func<CallSite, object, Microsoft.Office.Interop.Excel.Worksheet>>.Create(Microsoft.CSharp.RuntimeBinder.Binder.Convert(CSharpBinderFlags.ConvertExplicit, typeof(Microsoft.Office.Interop.Excel.Worksheet), typeof(frmBHD)));
+                    }
+                    Microsoft.Office.Interop.Excel.Worksheet worksheet = frmBHD.o__SiteContainer0.p__Site1.Target(frmBHD.o__SiteContainer0.p__Site1, workbook.Worksheets.Add(value, value, value, value));
+                    worksheet.Name = "data";
+                    worksheet.Cells[1, 1] = "车间安排统计数据";
+                    for (int i = 0; i < list.Count; i++)
+                    {
+                        worksheet.Cells[2, i + 1] = list[i];
+                    }
+                    Microsoft.Office.Interop.Excel.Range arg_1F8_0 = worksheet.get_Range("A2", value);
+              
+                    ((Microsoft.Office.Interop.Excel.Range)worksheet.Rows[3, Missing.Value]).ColumnWidth = 25;
+                    Microsoft.Office.Interop.Excel.Range range = null;
+                    int count = myTable.Rows.Count;
+                    int j = 1;
+                    int num = 1;
+                    int count2 = myTable.Columns.Count;
+                    object[,] array2 = new object[num, count2];
+                    try
+                    {
+                        int num2 = num;
+                        while (j < count)
+                        {
+                            if (count - j < num)
+                            {
+                                num2 = count - j;
+                            }
+                            for (int k = 0; k < num2; k++)
+                            {
+                                for (int l = 0; l < count2; l++)
+                                {
+                                    array2[k, l] = myTable.Rows[k + j][l].ToString();
+                                }
+                                System.Windows.Forms.Application.DoEvents();
+                            }
+                            range = worksheet.get_Range("A" + (j + 2).ToString(), ((char)(65 + count2 - 1)).ToString() + (j + num2 + 1).ToString());
+                            range.Value2 = array2;
+                            j += num2;
+                        }
+                        worksheet.SaveAs(fileName, value, value, value, value, value, Microsoft.Office.Interop.Excel.XlSaveAsAccessMode.xlNoChange, value, value, value);
+                        Marshal.ReleaseComObject(range);
+                        this.KillSpecialExcel(application);
+                        MessageBox.Show("导出数据成功！", "温馨提示", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                    }
+                    catch (Exception ex)
+                    {
+                        MessageBox.Show(ex.Message);
+                        return;
+                    }
+                    Thread.CurrentThread.CurrentCulture = currentCulture;
+                }
+            }
+        }
+        //点击导出
+        private void btnexport_Click(object sender, EventArgs e)
+        {
+            ToExcel(new SaveFileDialog());
+        }
+
+    }
+}
