6e28b0dddd38a6262b898e86e72737e8fb233592
dengni1 <2117207138@qq.com>
2019-09-03 18:00:26 +0800

    1111

--- OcdIcmoSplit/frmBHD.cs
+++ OcdIcmoSplit/frmBHD.cs
@@ -229,7 +229,7 @@ namespace OcdIcmoSplit
         private void btnexport_Click(object sender, EventArgs e)
         {
             ToExcel(new SaveFileDialog());
-            //1111
+            //122
         }
 
     }
